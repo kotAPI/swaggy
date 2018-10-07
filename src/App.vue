19eac57f19f4103a4309c68095f5daacf39795a4
Pranay Kothapalli <pranaykothapalli@gmail.com>
2018-10-07 12:12:07 +0530

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <div class="container">
+      <div class="navbar">
+        <div class="navbar-logo">Swaggy</div>
+      </div>
        <router-view></router-view>
     </div>
   
@@ -19,12 +22,33 @@ export default {
 </script>
 
 <style>
+.navbar-logo{
+  color:orange;
+  font-weight: 600;
+  font-size:2em;
+  padding-top:10px;
+  padding-left:50px;
+  font-family: 'Poppins',sans-serif;
+ 
+}
+.navbar{
+  width:100%;
+  position:fixed;
+  height:60px;
+  border-bottom:1px solid rgba(0,0,0,0.1);
+  top:0px;
+  left:0px;
+   background-color:white;
+  z-index:100;
+}
 .container{
   width:1160px;
   margin:auto;
-  border:1px solid black;
-  margin-top:60px;
+  z-index: 10;
+  position: relative;
+  margin-top:120px;
   padding:10px;
   font-family: 'Poppins', sans-serif !important;
+  margin-bottom:100px;
 }
 </style>
